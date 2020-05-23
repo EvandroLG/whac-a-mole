8889ce0d6e96220468eee6895060a861461532d2
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-23 21:48:08 +0200

    Updates

---
+++ precache-manifest.adb180064bda78d28a52bee4e68878b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86011034ee600a8865d77d9a48c88efa",
+    "url": "/whac-a-mole/index.html"
+  },
+  {
+    "revision": "72fcfc10a862008a7ac6",
+    "url": "/whac-a-mole/static/css/main.3e055360.chunk.css"
+  },
+  {
+    "revision": "ddaee2097701a7222624",
+    "url": "/whac-a-mole/static/js/2.1a398855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whac-a-mole/static/js/2.1a398855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fcfc10a862008a7ac6",
+    "url": "/whac-a-mole/static/js/main.e14695d9.chunk.js"
+  },
+  {
+    "revision": "b602fff799fb86426679",
+    "url": "/whac-a-mole/static/js/runtime-main.d3cba077.js"
+  },
+  {
+    "revision": "8b58ae6109776480e317c82bd26b9ba7",
+    "url": "/whac-a-mole/static/media/mole.8b58ae61.png"
+  }
+]);
