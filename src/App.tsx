fbcaf86de48a4e853bf6e9924b16e68806a1f111
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-23 12:34:48 +0200

    add logic to finish the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,49 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import useTimer from './useTimer';
 import useRandomSquare from './useRandomSquare';
 import { range } from 'ramda';
 import './App.css';
 
+const isOver = (counter: number) => counter === 0;
+
 function App() {
   const [score, setScore] = useState(0);
-  const square = useRandomSquare();
+  const [square, stopRandomSquare] = useRandomSquare();
   const counter = useTimer(60);
   const updateScore = useCallback(
     (key) => key === square && setScore(score + 1),
     [square, score]
   );
 
+  useEffect(() => {
+    if (isOver(counter)) {
+      stopRandomSquare();
+    }
+  }, [counter, stopRandomSquare]);
+
   return (
     <div className="App">
       <h1>Whack-A-Mole</h1>
-      <div className="App-score">{score}</div>
-      <div className="App-time">{counter}</div>
-      <div className="App-grid">
-        {range(0, 9).map((key) => (
-          <div
-            onClick={() => updateScore(key)}
-            key={key}
-            className={`App-square ${key === square && 'App-square--mole'}`}
-          ></div>
-        ))}
-      </div>
+      {isOver(counter) ? (
+        <>
+          <h2>Game over</h2>
+          <div className="App-score">{score}</div>
+        </>
+      ) : (
+        <>
+          <div className="App-score">{score}</div>
+          <div className="App-time">{counter}</div>
+          <div className="App-grid">
+            {range(0, 9).map((key) => (
+              <div
+                onClick={() => updateScore(key)}
+                key={key}
+                className={`App-square ${key === square && 'App-square--mole'}`}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/useRandomSquare.ts
+++ src/useRandomSquare.ts
@@ -1,16 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { compose } from 'ramda';
 
 const getRandomSquare = compose(Math.floor, () => Math.random() * 9);
 
 const useRandomSquare = () => {
   const [square, setSquare] = useState(getRandomSquare());
-
-  useEffect(() => {
-    setInterval(() => setSquare(getRandomSquare()), 1000);
+  const interval = useRef(0);
+  const stopRandomSquare: () => void = useCallback(() => {
+    clearInterval(interval.current);
   }, []);
 
-  return square;
+  useEffect(() => {
+    interval.current = window.setInterval(
+      () => setSquare(getRandomSquare()),
+      1000
+    );
+  }, []);
+
+  return [square, stopRandomSquare] as const;
 };
 
 export default useRandomSquare;
